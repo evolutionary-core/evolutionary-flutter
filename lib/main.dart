a3ad19806eca365e49a6bba09c64b694e1a53cfc
Rishabh Chawla <rishabh31121999@gmail.com>
2019-07-10 15:06:10 +0530

    chore: use state in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'src/screens/homepage.dart';
 import 'dart:convert';
+import 'src/state.dart';
 
 void main() => runApp(Evolutionary());
 
@@ -9,18 +10,11 @@ Future getSpecieData(String name, BuildContext context) async {
   return json.decode(data);
 }
 
-final List<String> animals = ['fish', 'reptiles', 'mammals', 'birds', 'insets', 'humans'];
-final String title = "Evolutionary";
-final ThemeData theme = ThemeData(
-  primaryColor: Color(0xFF118855),
-  accentColor: Colors.yellow,
-);
-
 class Evolutionary extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: title, theme: theme, home: Scaffold(
-      appBar: AppBar(title: Text(title), elevation: 0,),
+    return MaterialApp(title: Evo.title, theme: Evo.theme, home: Scaffold(
+      appBar: AppBar(title: Text(Evo.title), elevation: 0,),
       body: HomePage()
     ));
   }
