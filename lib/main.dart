7903038ff7ad6362e184ec84c3ce6bc0a145125e
Rishabh Chawla <rishabh31121999@gmail.com>
2019-07-10 17:22:22 +0530

    feat: initial navigation

--- lib/main.dart
+++ lib/main.dart
@@ -7,9 +7,14 @@ void main() => runApp(Evolutionary());
 class Evolutionary extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: Evo.title, theme: Evo.theme, home: Scaffold(
-      appBar: AppBar(title: Text(Evo.title), elevation: 0,),
-      body: HomePage()
-    ));
+    return MaterialApp(title: Evo.title, theme: Evo.theme,
+    initialRoute: '/',
+    routes: {
+      '/': (context) => Scaffold(
+        appBar: AppBar(title: Text(Evo.title), elevation: 0,),
+        body: HomePage(),
+      )
+    }
+    );
   }
 }
