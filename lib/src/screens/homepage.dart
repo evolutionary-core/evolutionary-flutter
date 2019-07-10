3132b2b20b0bc82a2e4412166346e4d5b45ea904
Rishabh Chawla <rishabh31121999@gmail.com>
2019-07-10 20:02:16 +0530

    feat: made homepage lovely

--- lib/src/screens/homepage.dart
+++ lib/src/screens/homepage.dart
@@ -17,23 +17,75 @@ class HomePage extends StatelessWidget {
             ],
         ),
         constraints: BoxConstraints.expand(
-        height: 300.0
+        height: 250.0
+      )),
+      Center(child: Container(
+        transform: Matrix4.translationValues(0, -40, 0),
+        constraints: BoxConstraints.expand(
+          height: 80,
+          width: MediaQuery.of(context).size.width - 30,
+        ),
+        child: Card(elevation: 5,
+          child: Padding(padding: EdgeInsets.fromLTRB(10, 0, 10, 10),
+            child:  TextField(
+              decoration: InputDecoration(
+                labelText: "Search Species"
+              ),
+            ),
+          )
+        ),
       )),
       Center(
-        child: Container(
-            transform: Matrix4.translationValues(0, -40, 0),
-            constraints: BoxConstraints.expand(
-              height: 80,
-              width: MediaQuery.of(context).size.width * 0.85,
-            ),
-            child: Card( // TODO: Add statefull widget for animal selection
-                color: Colors.white,
-                elevation: 10,
-                child: Text("Apple"), 
-            ),
-        ),
+        child: Text("Hot Categories",
+          style: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.w600,            
+          ),
+        )
       ),
+      Container(
+            constraints: BoxConstraints.expand(
+              height: 220,
+            ),
+            child: categoriesBuilder(context)
+        ),
 
     ],);
   }
+}
+
+ListView categoriesBuilder(context) {
+  return ListView.builder(
+    padding: EdgeInsets.fromLTRB(50, 10, 0, 0),
+    scrollDirection: Axis.horizontal,
+    itemCount: Evo.animals.length,
+    itemBuilder: (context, index) {
+      return Container(
+          constraints: BoxConstraints.expand(
+            width: 200,
+            height: 500,
+          ),
+          child: Card(
+            color: Colors.white,
+            elevation: 1,
+            clipBehavior: Clip.antiAlias,
+            child: Padding(padding: EdgeInsets.fromLTRB(0, 0, 0, 20),
+              child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Image.asset(
+                    '${Evo.animalImages[index]}',
+                    height: 150,
+                    fit: BoxFit.cover
+              ),
+              Text('${Evo.animals[index]}'.toUpperCase(),
+                textAlign: TextAlign.start,
+              )
+          ],)
+            )
+        ),
+      );
+    },
+  );
+  
 }
