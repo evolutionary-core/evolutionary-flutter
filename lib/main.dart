4865e82a78275e8abe50cc5e8a1b9e1ecbf21219
Rishabh Chawla <rishabh31121999@gmail.com>
2019-07-10 13:28:54 +0530

    chore: keep only homepage in the homepage.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,27 @@
 import 'package:flutter/material.dart';
 import 'src/screens/homepage.dart';
+import 'dart:convert';
 
 void main() => runApp(Evolutionary());
 
+Future getSpecieData(String name, BuildContext context) async {
+  String data = await DefaultAssetBundle.of(context).loadString("data/"+name+".json");
+  return json.decode(data);
+}
 
+final List<String> animals = ['fish', 'reptiles', 'mammals', 'birds', 'insets', 'humans'];
+final String title = "Evolutionary";
+final ThemeData theme = ThemeData(
+  primaryColor: Color(0xFF118855),
+  accentColor: Colors.yellow,
+);
 
+class Evolutionary extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(title: title, theme: theme, home: Scaffold(
+      appBar: AppBar(title: Text(title), elevation: 0,),
+      body: HomePage()
+    ));
+  }
+}
--- lib/src/screens/homepage.dart
+++ lib/src/screens/homepage.dart
@@ -1,10 +1,4 @@
 import 'package:flutter/material.dart';
-import 'dart:convert';
-
-Future getSpecieData(String name, BuildContext context) async {
-  String data = await DefaultAssetBundle.of(context).loadString("data/"+name+".json");
-  return json.decode(data);
-}
 
 final List<String> animals = ['fish', 'reptiles', 'mammals', 'birds', 'insets', 'humans'];
 final String title = "Evolutionary";
@@ -12,17 +6,6 @@ final ThemeData theme = ThemeData(
   primaryColor: Color(0xFF118855),
   accentColor: Colors.yellow,
 );
-
-class Evolutionary extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(title: title, theme: theme, home: Scaffold(
-      appBar: AppBar(title: Text(title), elevation: 0,),
-      body: HomePage()
-    ));
-  }
-}
-
 
 class HomePage extends StatelessWidget {
   @override
