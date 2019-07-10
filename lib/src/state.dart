c5fe1612594dd6e0d1d4f85f14bb21ab00e8ecbf
Rishabh Chawla <rishabh31121999@gmail.com>
2019-07-10 15:04:50 +0530

    feat: add fields to state

---
+++ lib/src/state.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+class Evo {
+  static List<String> animals = ['fish', 'reptiles', 'mammals', 'birds', 'insets', 'humans'];
+  static String title = "Evolutionary";
+  static ThemeData theme = ThemeData(
+    primaryColor: Color(0xFF118855),
+    accentColor: Colors.yellow,
+  );
+}
